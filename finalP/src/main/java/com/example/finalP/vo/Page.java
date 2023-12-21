26eabc6f553e2cd2be98501f7e3b73d87c655e8e
user <user@DESKTOP-V2R5CA8>
2023-12-21 15:01:58 +0900

    [박겨레]		vo패키지 수정
    		page 클래스 추가
    		리소스 오탈자 수정

---
+++ finalP/src/main/java/com/example/finalP/vo/Page.java
@@ -0,0 +1,16 @@
+package com.example.finalP.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Page {
+	private int page;
+	private String text;
+	private boolean isActive;
+}
