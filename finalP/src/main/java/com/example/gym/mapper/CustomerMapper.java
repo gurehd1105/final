95b88399c34a69759ebd6dc60c5c89e316b67a8a
jssjssj <148733552+jssjssj@users.noreply.github.com>
2023-12-22 17:23:27 +0900

    [김지산] [feat] customer crud 진행
    		- CustomerMapper.xml , CustomerMapper.java,
    		  CustomerService.java , CustomerController.java
    		  쿼리 작성 및 프로세스 세팅 / 진행 중

--- finalP/src/main/java/com/example/gym/mapper/CustomerMapper.java
+++ finalP/src/main/java/com/example/gym/mapper/CustomerMapper.java
@@ -1,8 +1,24 @@
 package com.example.gym.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.gym.vo.Customer;
 
 @Mapper
 public interface CustomerMapper {
-
+	// select
+	Customer loginCustomer(Customer customer);
+	
+	// insert (가입) + 선택정보 (Image)
+	int insertCustomer(Map<String, Object> paramMap);
+	int insertCustomerDetail(Map<String, Object> paramMap);
+	int insertCustomerImg(Map<String, Object> paramMap);
+	
+	// 탈퇴 update(Active) + delete(Image, Detail)
+	int updateCustomerActive(Customer customer);
+	int deleteCustomerImg(Customer customer);
+	int deleteCustomerDetail(Customer customer);
+	
 }
--- finalP/src/main/java/com/example/gym/service/CustomerService.java
+++ finalP/src/main/java/com/example/gym/service/CustomerService.java
@@ -1,10 +1,33 @@
 package com.example.gym.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.gym.mapper.CustomerMapper;
+import com.example.gym.vo.Customer;
 
 @Service
 @Transactional
 public class CustomerService {
-
+	@Autowired private CustomerMapper customerMapper;
+	
+	public Customer loginCustomer(Customer customer){
+		
+		customer = customerMapper.loginCustomer(customer);
+		
+		return customer;
+	}
+	
+	public int insertCustomer(Map<String, Object> paramMap) {
+		int result = 0;
+		int row0 = customerMapper.insertCustomer(paramMap);
+		int row1 = customerMapper.insertCustomerDetail(paramMap);
+		if(row0>1 && row1 >1) {
+			result = 1;
+		}
+		return result;
+	}
 }
