ea1ccc135b6db10cef5708bff3fc8c14cafaff7e
GD <wnrkgml0514@naver.com>
2023-12-22 17:33:58 +0900

    [주가희][feat]sportsEquipmentMapper interface 생성

---
+++ finalP/src/main/java/com/example/gym/mapper/SportsEquipmentMapper.java
@@ -0,0 +1,35 @@
+package com.example.gym.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.gym.vo.SportsEquipment;
+import com.example.gym.vo.SportsEquipmentImg;
+
+@Mapper
+public interface SportsEquipmentMapper {
+	
+	//sportsEquipment 추가
+	int insertSportsEquipment(SportsEquipment sportsEuipment);
+	
+	//sportsEquipmentImg 추가
+	int insertSportsEquipmentImg(SportsEquipmentImg sportsEquipmentImg);
+	
+	//sportsEquipment 목록 + 검색 + 페이징
+	List<Map<String,Object>> selectSportsEquipment(Map<String,Object> paramMap);
+	
+	//sportsEquipment 상세보기(selectSportsEquipmentOne + selectSportsEquipmentImgOne)
+	SportsEquipment selectSportsEquipmentOne(int sportsEquipmentNo);
+	List<SportsEquipmentImg> selectSportsEquipmentImgList(int sportsEquipmentNo);
+	
+	//sportsEquipment 삭제를 위해 sportsEquipmentImg 삭제
+	int deleteSportsEquipmentImg(int sportsEquipmentNo);
+	
+	//sportsEquipment 삭제
+	int deleteSportsEquipment(int sportsEquipmentNo);
+	
+	//sportsEquipmentImg 개별 삭제
+	int deleteOneSportsEquipmentImg(int sportsEquipmentImgNo);
+}
