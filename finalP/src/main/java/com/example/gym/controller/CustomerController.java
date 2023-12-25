a61d67ea48297702a74520759adbe756390e01b9
user <user@DESKTOP-EVGBI8M>
2023-12-26 00:32:18 +0900

    [김지산] [feat] customer CRUD 진행 중
    		- customerMapper.xml , customerMapper.java,
    		  customerService.java, customerControler.java 수정
    
    		- 회원정보 수정 , password 수정프로세스 진행 중

--- finalP/src/main/java/com/example/gym/controller/CustomerController.java
+++ finalP/src/main/java/com/example/gym/controller/CustomerController.java
@@ -16,9 +16,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @Controller
 public class CustomerController {
-
   @Autowired
   private CustomerService customerService;
+  
+  // login (로그인) Form
+  @GetMapping("/loginCustomer")
+  public String loginCustomer() {
+    return "customer/loginCustomer";
+  }
+
+  // login 후 Act -> home.jsp로 이동
+  @PostMapping("/loginCustomer")
+  public String loginCustomer(Model model, HttpSession session, Customer customer) {
+    Customer loginCustomer = customerService.loginCustomer(customer);
+    
+    session.setAttribute("loginCustomer", loginCustomer);
+
+    return "home";
+  }
 
   // insert (회원가입) Form
   @GetMapping("/insertCustomer")
@@ -28,45 +43,43 @@ public class CustomerController {
 
   // insert (회원가입) Act
   @PostMapping("/insertCustomer")
-  public String insertCustomer(
-    Customer customer,
-    CustomerDetail customerDetail
-  ) {
-    Map<String, Object> paramMap = new HashMap<>();
-    paramMap.put("customer", customer);
-    paramMap.put("customerDetail", customerDetail);
-    System.err.println(paramMap); // 매개값 디버깅
-
-    customerService.insertCustomer(paramMap);
-    return "customer/loginCustomer";
+  public String insertCustomer(Customer customer,CustomerDetail customerDetail) {
+	customerService.insertCustomer(customer, customerDetail);
+	
+		return "customer/loginCustomer";	
   }
 
   // delete (탈퇴) update(customerActive : Y -> N), delete(customerImg , customerDetail)
   // 완료 후 loginForm으로 이동
+  @GetMapping("/deleteCustomer")
   public String deleteCustomer(Customer customer) {
     customerService.deleteCustomer(customer);
     return "customer/loginCustomer";
   }
-
-  // login (로그인) Form
-  @GetMapping("/loginCustomer")
-  public String loginCustomer() {
-    return "customer/loginCustomer";
+  
+  @GetMapping("/customerOne")
+  public String customerOne(Customer customer, HttpSession session, Model model) {
+	  customer = (Customer)session.getAttribute("loginCustomer");
+	  Map<String, Object> resultMap = customerService.customerOne(customer);
+	  model.addAttribute("resultMap", resultMap);
+	  
+	  return "customer/customerOne";
   }
-
-  // login 후 Act -> home.jsp로 이동
-  @PostMapping("/loginCustomer")
-  public String loginCustomer(
-    Model model,
-    HttpSession session,
-    Customer paramCustomer
-  ) {
-    Customer loginCustomer = customerService.loginCustomer(paramCustomer);
-    session.setAttribute("loginCustomer", loginCustomer);
-
-    return "home";
+  
+  @GetMapping("/updateCustomerOne")
+  public String updateCustomerOne(Customer customer, HttpSession session, Model model) {
+	 
+	  
+	  return "customer/updateCustomerOne";
   }
-
+  
+  
+  @PostMapping("/updateCUstomerOne")
+  public String updateCustomerOne(CustomerDetail customerDetail) {
+	  
+	  return "redirect:/updateCustomerOne";
+  }
+  
   // logout -> login.jsp로 이동
   @GetMapping("/logoutCustomer")
   public String logoutCustomer(HttpSession session) {
--- finalP/src/main/java/com/example/gym/service/CustomerService.java
+++ finalP/src/main/java/com/example/gym/service/CustomerService.java
@@ -8,47 +8,59 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.gym.mapper.CustomerMapper;
 import com.example.gym.vo.Customer;
+import com.example.gym.vo.CustomerDetail;
 
 @Service
 @Transactional
 public class CustomerService {
 	@Autowired private CustomerMapper customerMapper;
 	
-	// Image 등록
-		public int insertCustomerImg(Map<String, Object> paramMap) {
-			// 타 customer 관련 완료 후 fileUpload폼 생성 및 구현 예정
-			
-			return 0;		
-		}
-	
 	// 로그인
-	public Customer loginCustomer(Customer customer){
+	public Customer loginCustomer(Customer paramCustomer){
 		
-		customer = customerMapper.loginCustomer(customer);
+		Customer loginCustomer = customerMapper.loginCustomer(paramCustomer);
 		
-		return customer;
+		return loginCustomer;
 	}
 	
 	// 회원가입
-	public int insertCustomer(Map<String, Object> paramMap) {
+	public int insertCustomer(Customer paramCustomer, CustomerDetail paramCustomerDetail) {
 		int result = 0;
-		int row0 = customerMapper.insertCustomer(paramMap);
-		int row1 = customerMapper.insertCustomerDetail(paramMap);
-		if(row0>1 && row1 >1) {
+		int row = customerMapper.insertCustomer(paramCustomer);
+		int row2 = customerMapper.insertCustomerDetail(paramCustomerDetail);
+		
+		if(row >0 && row2>0) {
 			result = 1;
 		}
 		return result;
 	}
 	
+	// Image 등록
+	public void insertCustomerImg() {
+		// 타 customer 관련 완료 후 fileUpload폼 생성 및 구현 예정
+			
+	}
+	
 	// 탈퇴
-	public int deleteCustomer(Customer customer) {
+	public int deleteCustomer(Customer paramCustomer) {
 		int result = 0;
-		int row0 = customerMapper.updateCustomerActive(customer);
-		int row1 = customerMapper.deleteCustomerImg(customer);
-		int row2 = customerMapper.deleteCustomerDetail(customer);
-		if(row0>0 && row1>0 && row2>0) {
+		int row = customerMapper.updateCustomerActive(paramCustomer);
+		int row2 = customerMapper.deleteCustomerImg(paramCustomer);
+		int row3 = customerMapper.deleteCustomerDetail(paramCustomer);
+		if(row>0 && row2>0 && row3>0) {
 			result = 1;
 		}
+		return result;
+	}
+	
+	// 상세보기
+	public Map<String, Object> customerOne(Customer paramCustomer) {
+		Map<String, Object> resultMap = customerMapper.customerOne(paramCustomer);
+		return resultMap;
+	}
+	
+	public int updateCustomerOne(CustomerDetail customerDetail) {
+		int result = customerMapper.updateCustomerOne(customerDetail);
 		return result;
 	}
 }
