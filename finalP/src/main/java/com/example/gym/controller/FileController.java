5426eb03330028e05ae214a5fca951577d2d8d7b
jssjssj <148733552+jssjssj@users.noreply.github.com>
2024-02-06 11:18:47 +0900

    [김지산] FileController 수정

--- finalP/src/main/java/com/example/gym/controller/FileController.java
+++ finalP/src/main/java/com/example/gym/controller/FileController.java
@@ -23,31 +23,11 @@ public class FileController {
 	@PostMapping("upload/{folder}")
 	@ResponseBody
 	public String upload(@RequestParam("file") MultipartFile file, HttpSession session, @PathVariable String folder) {
-		//String path = session.getServletContext().getRealPath("/upload/" + folder);
-		String path2 = "C:\\Users\\GD\\git\\final\\finalP\\src\\main\\webapp\\upload\\"+folder;
-		String path3 = "/home/ubuntu/upload/" + folder;
-		log.info(path3 + " is path3");
-
-	
-		File f = new File(path3);
-         
-		if (!f.exists()) {
-		    // 폴더가 존재하지 않으면 생성
-		    log.info("path2 does not exist");
-		    boolean success = f.mkdirs(); // 디렉토리 생성
-		    if (success) {
-		        log.info("Successfully created directory: " + path3);
-		    } else {
-		        log.error("Failed to create directory: " + path3);
-		    }
-		} else {
-		    // 폴더가 이미 존재할 경우
-		    log.info("path3 already exists 이게 떠야 돼 제발");
+		String path = session.getServletContext().getRealPath("/upload/" + folder);
+		{
+			File f = new File(path);
+			if (!f.exists()) f.mkdir();
 		}
-		 
-		 
-		 
-			
 		if (file.getSize() != 0) {
 			// fileName 값 세팅
 			String fileName = UUID.randomUUID().toString();
@@ -55,15 +35,12 @@ public class FileController {
 			String fileName2 = oName.substring(oName.lastIndexOf("."));
 
 			// path 저장
-			File copy = new File(path2 + "\\" + fileName + fileName2);
-			log.info(copy.toString() + "	is copy.toString()");
+			File copy = new File(path + "/" + fileName + fileName2);
+			log.info(copy.toString());
 			try {
-				log.info("Start file.transfer");
 				file.transferTo(copy);
-				log.info(copy.getName() + " is copy.getName");
 				return copy.getName();
 			} catch (IllegalStateException | IOException e) {
-				log.info("Fail file.transfer");
 				e.printStackTrace();
 			}
 		}
